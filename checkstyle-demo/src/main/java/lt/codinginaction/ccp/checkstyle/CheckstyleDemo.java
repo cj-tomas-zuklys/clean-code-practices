962099b371f144630e40bf1edcbaf81ea1b6b94a
Tomas Zuklys <tomas.zuklys@gmail.com>
2017-08-01 18:58:34 +0300

    Checktyle Demo Project - initial commit

---
+++ checkstyle-demo/src/main/java/lt/codinginaction/ccp/checkstyle/CheckstyleDemo.java
@@ -0,0 +1,11 @@
+package lt.codinginaction.ccp.checkstyle;
+
+public final class CheckstyleDemo {
+
+	private CheckstyleDemo() { }
+
+	public static void main(String[] args) {
+		System.out.println("Checkstyle demo");
+	}
+
+}
